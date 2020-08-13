f00b6e52ff94d6ffed7f2e2f0fea9e6ee75a8970
Alley Purcell <zanastar@gmail.com>
2020-08-13 00:30:10 -0500

    Week 2: Exercise: Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -57,6 +58,29 @@ const ContactNavigator = createStackNavigator(
       },
       headerLeft: <Icon
         name='address-card'
+        type='font-awesome'
+        iconStyle={styles.stackIcon}
+        onPress={() => navigation.toggleDrawer()}
+      />
+    })
+  }
+);
+
+const ReservationNavigator = createStackNavigator(
+  {
+    Reservation: { screen: Reservation },
+  },
+  {
+    navigationOptions: ({navigation}) => ({
+      headerStyle: {
+        backgroundColor: '#5637DD'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        color: '#fff'
+      },
+      headerLeft: <Icon
+        name='tree'
         type='font-awesome'
         iconStyle={styles.stackIcon}
         onPress={() => navigation.toggleDrawer()}
@@ -156,6 +180,20 @@ const MainNavigator = createDrawerNavigator(
         drawerIcon: ({tintColor}) => (
           <Icon
             name='list'
+            type='font-awesome'
+            size={24}
+            color={tintColor}
+          />
+        )
+      }
+    },
+    Reservation: {
+      screen: ReservationNavigator,
+      navigationOptions: {
+        drawerLabel: 'Reserver Campsite',
+        drawerIcon: ({tintColor}) => (
+          <Icon
+            name='tree'
             type='font-awesome'
             size={24}
             color={tintColor}
