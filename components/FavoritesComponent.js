b9fd9c92202b21593cd05439e06d9e11ed31e058
Alley Purcell <zanastar@gmail.com>
2020-08-16 19:24:28 -0500

    Week 3: Exercise: Swipeout and Deleting Favorites

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -4,12 +4,19 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
+
 
 const mapStateToProps = state => {
   return {
     campsites: state.campsites,
     favorites: state.favorites
   };
+};
+
+const mapDispatchToProps = {
+  deleteFavorite: campsiteId => (deleteFavorite(campsiteId))
 };
 
 class Favorites extends Component {
@@ -22,13 +29,22 @@ class Favorites extends Component {
 
     const { navigate } = this.props.navigation;
     const renderFavoriteItem = ({item}) => {
+      const rightButton = [
+        {
+          text: 'Delete',
+          type: 'delete',
+          onPress: () => this.props.deleteFavorite(item.id)
+        }
+      ];
       return (
+       <Swipeout right={rightButton} autoClose={true}>
         <ListItem
-          title={item.name}
-          subtitle={item.description}
-          leftAvatar={{source: {uri: baseUrl + item.image}}}
-          onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
-        />
+            title={item.name}
+            subtitle={item.description}
+            leftAvatar={{source: {uri: baseUrl + item.image}}}
+            onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
+          />
+       </Swipeout>
       );
     };
 
@@ -56,4 +72,4 @@ class Favorites extends Component {
   }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
