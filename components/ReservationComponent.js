3c541b8a3b2017f4a4fa6b4559d1b677c597df77
Alley Purcell <zanastar@gmail.com>
2020-08-22 13:35:29 -0500

    Week 3 Workshop Assignment: Task 1

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal } from 'react-native';
 import DatePicker from 'react-native-datepicker';
+import * as Animatable from 'react-native-animatable';
 
 class Reservation extends Component {
 
@@ -39,7 +40,7 @@ class Reservation extends Component {
 
   render() {
     return (
-      <ScrollView>
+      <Animatable.View animation="zoomIn" delay={1000} duration={2000}>
         <View style={styles.formRow}>
           <Text style={styles.formLabel}>Number of Campers</Text>
           <Picker
@@ -116,7 +117,7 @@ class Reservation extends Component {
             />
           </View>
         </Modal>
-      </ScrollView>
+      </Animatable.View>
     )
   }
 }
