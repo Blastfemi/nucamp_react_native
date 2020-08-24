d73987ca97a26ec11ee8605257ca994787301856
Alley Purcell <zanastar@gmail.com>
2020-08-23 20:05:23 -0500

    Week 4 Workshop Assignment: Task 1

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -4,6 +4,7 @@ import { Input, CheckBox, Button, Icon} from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
+import * as ImageManipulator from 'expo-image-manipulator';
 import { createBottomTabNavigator } from 'react-navigation';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -153,11 +154,20 @@ class RegisterTab extends Component {
       });
       if (!capturedImage.cancelled) {
         console.log(capturedImage);
-        this.setState({imageUrl: capturedImage.uri})
+        this.processImage(capturedImage.uri)
       }
     }
   }
-  
+
+  processImage = async (imgUri) => {
+    const processedImage = await ImageManipulator.manipulateAsync(imgUri,
+      [{ resize: {width: 400 } }],
+      { format: ImageManipulator.SaveFormat.PNG }
+      )
+      console.log(processedImage);
+      this.setState({ imageUrl: processedImage.uri })
+    }
+    
   handleRegister() {
     console.log(JSON.stringify(this.state));
     if (this.state.remember) {
